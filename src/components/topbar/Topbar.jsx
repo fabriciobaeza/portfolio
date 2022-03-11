14a0f3432f59598e439a353e8c6229ddcaa45ec3
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-10 23:16:24 -0500

    Material-UI

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import './topbar.scss'
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
@@ -6,6 +7,14 @@ export default function Topbar() {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className='logo'>genius.</a>
+          <div className="itemContainer">
+          <Person className='icon'/>
+          <span>347-123-4567</span>
+          </div>
+          <div className="itemContainer">
+          <Mail className='icon'/>
+          <span>fabricio@gmail.com</span>
+          </div>
         </div>
         <div className="right">
         </div>
