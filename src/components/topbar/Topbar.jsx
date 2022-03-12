afed02c0c196dbbc242c0e5af027ecc885d1c7da
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-11 23:15:35 -0500

    Hamburger Menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,24 +1,31 @@
-import './topbar.scss'
-import {Person, Mail} from "@material-ui/icons"
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo'>genius.</a>
+          <a href="#intro" className="logo">
+            genius.
+          </a>
           <div className="itemContainer">
-          <Person className='icon'/>
-          <span>347-123-4567</span>
+            <Person className="icon" />
+            <span>347-123-4567</span>
           </div>
           <div className="itemContainer">
-          <Mail className='icon'/>
-          <span>fabricio@gmail.com</span>
+            <Mail className="icon" />
+            <span>fabricio@gmail.com</span>
           </div>
         </div>
         <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
