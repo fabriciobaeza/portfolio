bc7f1267342ae947f3b719a82d05d3634efc3a7d
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-17 22:08:04 -0400

    Portfolio Page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,60 @@
-import './portfolio.scss'
+import "./portfolio.scss";
 
 export default function Portfolio() {
   return (
-    <div className='portfolio' id='portfolio'>Portfolio</div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://yi-files.s3.eu-west-1.amazonaws.com/products/1189000/1189396/1982618-full.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
