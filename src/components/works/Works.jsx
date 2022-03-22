3ab36953321b770b9b32a7b80696276a02de22fd
Fabricio Baeza <fabriciobaeza@gmail.com>
2022-03-21 22:55:12 -0400

    Works Page Part 1

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,7 +1,25 @@
-import './works.scss'
+import "./works.scss";
 
 export default function Works() {
   return (
-    <div className='works' id='works'>Works</div>
-  )
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat saepe pariatur, praesentium sed voluptates, numquam amet, culpa fugit assumenda nulla dignissimos error magni eius explicabo incidunt nostrum et. Architecto, enim.</p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
